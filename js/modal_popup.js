d42098bf91c523dd9cc4f0ce2db92746f154b6f0
ju11z <fgtmnt@mail.ru>
2023-05-16 15:05:07 +0300

    started cat modal

--- js/modal_popup.js
+++ js/modal_popup.js
@@ -1,22 +1,16 @@
-var modal = document.getElementById("requestModal");
+var modal = document.getElementById("modal-window");
+var btn = document.getElementById("modal-popup-btn");
+var span = document.getElementById("modal-popup-btn-close");
 
-// Get the button that opens the modal
-var btn = document.getElementById("showRequestModal");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
