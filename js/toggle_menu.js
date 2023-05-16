ee6ba7842d40418b0acf6deffafd8259ede9f746
ju11z <fgtmnt@mail.ru>
2023-05-16 10:46:53 +0300

    menu item active

--- js/toggle_menu.js
+++ js/toggle_menu.js
@@ -1,16 +1,15 @@
 toggleButton=document.getElementById("toggle-menu-btn");
 menu=document.getElementById("toggle-menu");
+menu.style.display="none";
 
 toggleButton.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     if(menu.style.display=="block"){
         menu.style.display="none";
-        alert("off");
     }
     else{
         menu.style.display="block";
-        alert("on");
     }
 }
 
