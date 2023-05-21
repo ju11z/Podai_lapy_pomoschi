1d43c4e131db79ad1729815e6b685c4bae847b0e
ju11z <fgtmnt@mail.ru>
2023-05-21 12:10:51 +0300

    pagination style fixed

--- js/pagination.js
+++ js/pagination.js
@@ -2,7 +2,7 @@ const paginationNumbers = document.getElementById("pagination-numbers");
 const paginatedList = document.getElementById("paginated-list");
 const listItems = paginatedList.querySelectorAll(".card");
 
-const paginationLimit = 9;
+const paginationLimit = 6;
 const pageCount = Math.ceil(listItems.length / paginationLimit);
 let currentPage = 1;
 
